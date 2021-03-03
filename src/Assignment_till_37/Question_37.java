1b82c075c70bf8d310edaac60e12a0a3820c326d
Shashin21 <60948680+Shashin21@users.noreply.github.com>
2021-03-02 20:00:41 -0600

    assignment

--- src/Assignment_till_37/Question_37.java
+++ src/Assignment_till_37/Question_37.java
@@ -1,4 +1,18 @@
 package Assignment_till_37;
 
+import java.util.Scanner;
+
 public class Question_37 {
+    public static void main(String[] args) {
+        final int ROWS = 3;
+        final int COLUMNS = 3;
+        int [][] myArray = new int[ROWS][COLUMNS];
+        Scanner input = new Scanner(System.in);
+        for (int i =0; i<ROWS; i++){
+            for(int j =0; j<COLUMNS;j++){
+                System.out.println("Enter the number: ");
+                myArray[i][j] = input.nextInt();
+            }
+        }
+    }
 }
